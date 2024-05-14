aa370416b33627c01a73f9b02877a12d5c2f7c49
supermistral <mediamart37@gmail.com>
2024-05-14 19:58:50 +0300

    Fix keys naming in boto3 Session

--- backend/api/object_storage/session.py
+++ backend/api/object_storage/session.py
@@ -4,7 +4,7 @@ from ..config import settings
 
 
 SESSION = boto3.Session(
-    access_key_id=settings.OBJECT_STORAGE_ACCESS_KEY_ID,
-    secret_access_key=settings.OBJECT_STORAGE_SECRET_ACCESS_KEY,
+    aws_access_key_id=settings.OBJECT_STORAGE_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.OBJECT_STORAGE_SECRET_ACCESS_KEY,
     region_name=settings.OBJECT_STORAGE_REGION_NAME,
 )
