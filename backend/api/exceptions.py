d1a2fc388c4f2e29263cef527eec7fdaa3109406
supermistral <mediamart37@gmail.com>
2024-05-20 01:09:48 +0300

    Customize validation error

--- backend/api/exceptions.py
+++ backend/api/exceptions.py
@@ -1,7 +1,10 @@
 from typing import Any, Optional
 
+from pydantic import ValidationError
 from fastapi import HTTPException, Request
 from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+from sqlalchemy.exc import NoResultFound
 
 
 class BaseException(HTTPException):
@@ -9,19 +12,24 @@ class BaseException(HTTPException):
     Unified interface for handling custom errors
     """
 
+    STATUS: int = ...
     CODE: str = ...
+    MESSAGE: str = ...
 
     def __init__(
         self,
-        status_code: int,
-        message: str,
+        status_code: Optional[int] = None,
+        message: Optional[str] = None,
         details: Any = None,
         code: Optional[str] = None,
         **kwargs,
     ) -> None:
-        super().__init__(status_code, details, **kwargs)
+        status_code = status_code or self.STATUS
+
+        super().__init__(status_code, details)
+
         self.code = code or self.CODE
-        self.message = message
+        self.message = (message or self.MESSAGE).format(**kwargs)
         self.details = details
 
 
@@ -37,11 +45,45 @@ async def base_exception_handler(request: Request, exc: BaseException):
 
 
 class NotFoundException(BaseException):
+    STATUS = 404
     CODE = "NotFound"
+    MESSAGE = "Resource not found."
 
-    def __init__(self, filters: Optional[list[tuple[str, Any]]] = None) -> None:
+    def __init__(
+        self,
+        filters: Optional[list[tuple[str, Any]]] = None,
+        resource: Optional[str] = None,
+    ) -> None:
+        details = {"filters": filters, "resource": resource}
+        details = {k: v for k, v in details.items() if v is not None}
+
         super().__init__(
-            status_code=404,
-            message="Resource not found.",
-            details=filters,
+            details=details,
         )
+
+
+class RequestValidationException(BaseException):
+    STATUS = 400
+    CODE = "Validation"
+    MESSAGE = "The request failed during data validation."
+
+
+async def db_exception_handler(request: Request, exc: NoResultFound):
+    return await base_exception_handler(
+        request=request,
+        exc=NotFoundException()
+    )
+
+
+async def data_validation_exception_handler(request: Request, exc: RequestValidationError):
+    details = exc.errors()
+
+    for error in details:
+        error.pop("ctx")
+        error.pop("input")
+        error["message"] = error.pop("msg")
+
+    return await base_exception_handler(
+        request=request,
+        exc=RequestValidationException(details=details),
+    )
--- backend/api/main.py
+++ backend/api/main.py
@@ -1,14 +1,27 @@
 import uvicorn
 from fastapi import FastAPI, APIRouter
+from fastapi.exceptions import RequestValidationError
 from starlette.middleware.cors import CORSMiddleware
+from pydantic import ValidationError
 
 from .config import settings
 from .celery import create_app as create_celery_app
-from .exceptions import BaseException, base_exception_handler
+from .exceptions import (
+    BaseException,
+    NoResultFound,
+    base_exception_handler,
+    db_exception_handler,
+    data_validation_exception_handler,
+)
 from .documents.router import router as documents_router
 from .users.router import router as users_router
 from .operations.router import router as operations_router
+from .access.router import router as access_router
 from .session.middleware import SessionMiddleware
+from .access.exception_handlers import (
+    base_auth_jwt_exception_handler,
+    AuthJWTException,
+)
 
 
 def startup_actions() -> None:
@@ -41,10 +54,14 @@ def create_app() -> FastAPI:
     router.include_router(documents_router)
     router.include_router(users_router)
     router.include_router(operations_router)
+    router.include_router(access_router)
 
     app.include_router(router)
 
     app.add_exception_handler(BaseException, base_exception_handler)
+    app.add_exception_handler(AuthJWTException, base_auth_jwt_exception_handler)
+    app.add_exception_handler(NoResultFound, db_exception_handler)
+    app.add_exception_handler(RequestValidationError, data_validation_exception_handler)
 
     # Run actions on app startup
     startup_actions()
